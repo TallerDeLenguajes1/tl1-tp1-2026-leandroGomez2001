0f5e3e4f772720ba29dc58201a6f0e4d5530dad7
leandroGomez2001 <gomezleandroagustin@gmail.com>
2026-04-13 15:27:53 -0300

    segundo commit tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,14 @@
 int main() {
     printf("Hola mundo\n");
 
+    int variable = 14;
+    int *puntero = &variable;
+
+    printf("1. Contenido del puntero: %d\n", *puntero);
+    printf("2. Direccion almacenada por el puntero: %p\n", puntero);
+    printf("3. Direccion de memoria de la variable: %p\n", &variable);
+    printf("4. Direccion de memoria del puntero: %p\n", &puntero);
+    printf("5. Tamanio de la variable: %zu bytes\n", sizeof(variable));
+
     return 0;
 }
